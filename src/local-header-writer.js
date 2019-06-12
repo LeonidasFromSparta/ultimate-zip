b5ac4b461a7040b1cbe67e845e72f7ce55ca870d
Leonid Weinberg <leonwbrg@gmail.com>
2019-06-12 08:39:10 +0300

    dev

--- src/local-header-writer.js
+++ src/local-header-writer.js
@@ -4,33 +4,52 @@ import LocalHeaderSerializer from './local-header-serializer'
 export default class LocalHeaderWriteable extends Transform {
 
     deserializer = new LocalHeaderSerializer()
-    header
+    header = null
 
     _transform = (chunk, encoding, callback) => {
 
         // time for deserializing
-        if (this.header !== null)
-
         let bytesRead = 0
 
         while (bytesRead < chunk.length) {
 
-            const fixRead = this.deserializer.updateFixed(chunk.slice(bytesRead))
-            bytesRead += fixRead.bytes
+            // take care the header
+            if (this.header === null) {
 
-            if (!fixRead.done)
-                continue
+                const fixRead = this.deserializer.updateFixed(chunk.slice(bytesRead))
+                bytesRead += fixRead.bytes
 
-            const varRead = this.deserializer.updateVar(chunk.slice(bytesRead))
-            bytesRead += varRead.bytes
+                if (!fixRead.done)
+                    continue
 
-            if (varRead.done) {
+                const varRead = this.deserializer.updateVar(chunk.slice(bytesRead))
+                bytesRead += varRead.bytes
 
-                this.header = this.deserializer.deserealize()
-                this.chunk = chunk.slice(bytesRead)
+                if (varRead.done) {
 
-                debugger
-                break
+                    this.header = this.deserializer.deserealize()
+                    this.deserializer.reset()
+
+                    this.max = this.header.getCompressedSize()
+                    this.count = 0
+                } else {
+
+                    continue
+                }
+            }
+
+            // start flowing the data
+            if ((this.count + chunk.length - bytesRead) < this.max) {
+
+                this.push(chunk.slice(bytesRead))
+                this.count += (chunk.length - bytesRead)
+            } else {
+
+                const bytesRemaining = this.max - this.count
+                this.push(chunk.slice(bytesRead, bytesRead + bytesRemaining))
+                this.push(null)
+                bytesRead += (this.max - this.count)
+                this.header = null
             }
         }
 
