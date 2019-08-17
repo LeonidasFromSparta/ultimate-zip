86f5d9179bc48da3218cd40091a5a578bef12afa
Leonid Weinberg <leonwbrg@gmail.com>
2019-08-18 02:23:39 +0300

    run tests in band check

--- tests/integration/debug.js
+++ tests/integration/debug.js
@@ -1,13 +1,50 @@
 import UZip from './../../src/u-zip'
 
+const ASSETS_PATH = './tests/integration/assets'
+const EXTRACT_PATH = './tests/integration/assets/extracted'
+
+;
 (async () => {
 
     debugger
-    // await new UZip('./samples/7z-windows-normal.zip').extractArchive('./extr')
-    new UZip('./tests/integration/assets/win-7z-fast.zip').getEntries().then((entries)=> {
+    const keke = [
 
-        console.log(entries)
-        debugger
-    })
-    console.log('keke doine')
+    new UZip(ASSETS_PATH + '/win-7z-store.zip')
+    .extractArchive(EXTRACT_PATH + '/promise/win-7z-store'),
+
+    new UZip(ASSETS_PATH + '/win-7z-fast.zip')
+        .extractArchive(EXTRACT_PATH + '/promise/win-7z-fast'),
+
+    new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
+        .extractArchive(EXTRACT_PATH + '/promise/win-7z-fastest'),
+
+    new UZip(ASSETS_PATH + '/win-7z-normal.zip')
+        .extractArchive(EXTRACT_PATH + '/promise/win-7z-normal'),
+
+    new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
+        .extractArchive(EXTRACT_PATH + '/promise/win-7z-maximum'),
+
+    new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
+        .extractArchive(EXTRACT_PATH + '/promise/win-7z-ultra'),
+
+    new UZip(ASSETS_PATH + '/win-7z-store.zip')
+        .testArchive(),
+
+    new UZip(ASSETS_PATH + '/win-7z-fast.zip')
+        .testArchive(),
+
+    new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
+        .testArchive(),
+
+    new UZip(ASSETS_PATH + '/win-7z-normal.zip')
+        .testArchive(),
+
+    new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
+        .testArchive(),
+
+    new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
+        .testArchive()
+    ]
+
+    await Promise.all(keke)
 })()
