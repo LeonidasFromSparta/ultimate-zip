7312e9c73337ff359e84e0591daff4310c00e2e4
leonidw <leonid.weinberg@checkmarx.com>
2019-08-19 19:50:54 +0300

    change tests

--- tests/integration/promise-api.integration.test.js
+++ tests/integration/promise-api.integration.test.js
@@ -24,46 +24,50 @@ test('should assert promise extractArchive API', async () => {
 
     await new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
         .extractArchive(EXTRACT_PATH + '/promise/win-7z-ultra')
+
+    await new UZip(ASSETS_PATH + '/win-7z-store.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-fast.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-normal.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
+        .testArchive()
+
+    await new UZip(ASSETS_PATH + '/win-7z-store.zip')
+        .getEntries()
+
+    await new UZip(ASSETS_PATH + '/win-7z-fast.zip')
+        .getEntries()
+
+    await new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
+        .getEntries()
+
+    await new UZip(ASSETS_PATH + '/win-7z-normal.zip')
+        .getEntries()
+
+    await new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
+        .getEntries()
+
+    await new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
+        .getEntries()
 })
 
 test('should assert promise testArchive API', async () => {
 
-    await new UZip(ASSETS_PATH + '/win-7z-store.zip')
-        .testArchive()
 
-    await new UZip(ASSETS_PATH + '/win-7z-fast.zip')
-        .testArchive()
-
-    await new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
-        .testArchive()
-
-    await new UZip(ASSETS_PATH + '/win-7z-normal.zip')
-        .testArchive()
-
-    await new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
-        .testArchive()
-
-    await new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
-        .testArchive()
 })
 
 test('should assert promise getEntries API', async () => {
 
-    await new UZip(ASSETS_PATH + '/win-7z-store.zip')
-        .getEntries()
 
-    await new UZip(ASSETS_PATH + '/win-7z-fast.zip')
-        .getEntries()
-
-    await new UZip(ASSETS_PATH + '/win-7z-fastest.zip')
-        .getEntries()
-
-    await new UZip(ASSETS_PATH + '/win-7z-normal.zip')
-        .getEntries()
-
-    await new UZip(ASSETS_PATH + '/win-7z-maximum.zip')
-        .getEntries()
-
-    await new UZip(ASSETS_PATH + '/win-7z-ultra.zip')
-        .getEntries()
 })
